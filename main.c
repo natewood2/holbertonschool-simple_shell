eb45175282a9c78ae63b10947cfa6d94ccbdeefc
natewood2 <nathanwood.sfg@gmail.com>
2023-12-04 23:37:22 -0600

    added the buffer back to remain checking

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
             break;
         }
 
+        buffer[strcspn(buffer, "\n")] = 0;
         if (buffer[0] == '\0') 
         {
             continue;
