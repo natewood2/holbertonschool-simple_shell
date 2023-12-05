755c124a438d1b20b220f07759c0376aa150b15d
natewood2 <nathanwood.sfg@gmail.com>
2023-12-05 14:04:52 -0600

    Adding so additional waitpid check to hopefully fix the exit checks

--- execute_command.c
+++ execute_command.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-void execute_command(char *fullPath, char **args) 
-{
+void execute_command(char *fullPath, char **args) {
     pid_t pid;
     int status;
+    int exit_status;
 
     pid = fork();
     if (pid == 0) 
@@ -11,15 +11,22 @@ void execute_command(char *fullPath, char **args)
         if (execve(fullPath, args, environ) == -1) 
         {
             fprintf(stderr, "Failed to execute '%s'\n", fullPath);
-            exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE); 
         }
     } 
     else if (pid > 0) 
     {
         waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status)) 
+        {
+            exit_status = WEXITSTATUS(status);
+            exit(exit_status);
+        }
     } 
     else 
     {
         fprintf(stderr, "Failed to fork\n");
+        exit(EXIT_FAILURE);
     }
 }
