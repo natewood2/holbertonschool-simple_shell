9d81ae5d6f450c2b0b5b004e02264d39c2af405e
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 09:32:22 -0800

    Betty warning and error correction on execute command file

--- execute_command.c
+++ execute_command.c
@@ -10,20 +10,20 @@ int execute_command(char *command, char **args)
 	pid_t pid;
 	int status = 0;
 
-    pid = fork();
-    if (pid == 0)
-    {
-        execve(command, args, NULL);
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid > 0)
-    {
-        waitpid(pid, &status, 0);
-    }
-    else
-    {
-        perror("fork");
-    }
-    return (status);
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(command, args, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	}
+		waitpid(pid, &status, 0);
+	}
+	else
+	{
+		perror("fork");
+	}
+return (status);
 }
