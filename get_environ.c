b24d3c01396d962060575a97105cf6d308f1e110
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 10:08:24 -0800

    betty comments on get_environ

--- get_environ.c
+++ get_environ.c
@@ -1,24 +1,36 @@
 #include "main.h"
-
-char *get_environ(char **env) 
+/**
+ * get_environ - Get value of PATH environment vari
+ * @env: Pointer to array of environment vari
+ * Return: pointer to string of PATH or NULL
+ */
+char *get_environ(char **env)
 {
-    const char *pathPrefix = "PATH=";
-    const size_t prefixLen = strlen(pathPrefix);
-    char *path = NULL;
+	const char *pathPrefix = "PATH=";
+	const size_t prefixLen = strlen(pathPrefix);
+	char *path = NULL;
 
-    while (*env != NULL) 
-    {
-        if (strncmp(*env, pathPrefix, prefixLen) == 0) 
-        {
-            path = strdup(*env + prefixLen);
-            if (!path) 
-            {
-                perror("path");
-                exit(EXIT_FAILURE);
-            }
-            break;
-        }
-        env++;
-    }
-    return (path);
+	/* Loop through the environment variables */
+	while (*env != NULL)
+	{
+		/* Check if the current envir vari starts with "PATH=" */
+		if (strncmp(*env, pathPrefix, prefixLen) == 0)
+		{
+			/* Allocate memory for PATH value and copy it */
+			path = strdup(*env + prefixLen);
+
+			/*checks if memory allocatioon failed */
+			if (!path)
+			{
+			/* Print an error message and exit if allocation failes */
+			perror("path");
+			exit(EXIT_FAILURE);
+			}
+
+			/* If PATH is found, break out of loop */
+			break;
+		}
+		env++;
+	}
+	return (path);
 }
