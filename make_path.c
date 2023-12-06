f443de4ed306332b7f0c11ee38fed91fd488baad
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 10:20:39 -0800

    make_path.c betty error and comments

--- make_path.c
+++ make_path.c
@@ -1,13 +1,25 @@
 #include "main.h"
-
-char *construct_full_path(const char *dir, const char *cmd) 
+/**
+ * construct_full_path - Construct full path from dir and cmd
+ * @dir: Directory path
+ * @cmd: Command name
+ * Return: Pointer to string containing full path or NULL on failure
+ */
+char *construct_full_path(const char *dir, const char *cmd)
 {
-    char *full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-    if (full_path != NULL) 
-    {
-        strcpy(full_path, dir);
-        strcat(full_path, "/");
-        strcat(full_path, cmd);
-    }
-    return (full_path);
+	char *full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+
+	/* Check if memory allocation was successful */
+	if (full_path != NULL)
+	{
+		/* Copy directory path into full_path */
+		strcpy(full_path, dir);
+
+		/* Concatenate '/' to separate dir and cmd */
+		strcat(full_path, "/");
+
+		/* Concatenate command name to complete the full path */
+		strcat(full_path, cmd);
+	}
+	return (full_path);
 }
