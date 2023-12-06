1c5ac81050f4c1bc9493608ae4aaf526a7bcf746
KVang2 <144153433+KVang2@users.noreply.github.com>
2023-12-06 09:25:25 -0600

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,22 +1,27 @@
 #include "main.h"
-
-int execute_command(char *command, char **args) 
+/**
+ * execute_command - execution function
+ * @command: commands
+ * @args: args
+ * Return: return (status)
+*/
+int execute_command(char *command, char **args)
 {
-    pid_t pid;
-    int status = 0;
+	pid_t pid;
+	int status = 0;
 
     pid = fork();
-    if (pid == 0) 
+    if (pid == 0)
     {
         execve(command, args, NULL);
         perror("execve");
         exit(EXIT_FAILURE);
-    } 
-    else if (pid > 0) 
+    }
+    else if (pid > 0)
     {
         waitpid(pid, &status, 0);
-    } 
-    else 
+    }
+    else
     {
         perror("fork");
     }
