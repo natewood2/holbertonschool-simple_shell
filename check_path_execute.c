488f4b487e178d6e5fce4ce09e1618ce7c77e76c
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 09:26:54 -0800

    betty warning and error on check path file

--- check_path_execute.c
+++ check_path_execute.c
@@ -5,25 +5,24 @@
  * @args: arguments
  * Return: (1)
 */
-int check_path_and_execute(const char *cmd, char **args) 
+int check_path_and_execute(const char *cmd, char **args)
 {
-    char *path = get_environ(environ);
-    char *dir;
-    char commandPath[1024];
+	char *path = get_environ(environ);
+	char *dir;
+	char commandPath[1024];
 
-    dir = strtok(path, ":");
-    while (dir != NULL) 
-    {
-        sprintf(commandPath, "%s/%s", dir, cmd);
-        if (access(commandPath, X_OK) == 0) 
-        {
-            execute_command(commandPath, args);
-            free(path);
-            return (1);
-        }
-        dir = strtok(NULL, ":");
-    }
-
-    free(path);
-    return (0);
+	dir = strtok(path, ":");
+	while (dir != NULL)
+	{
+		sprintf(commandPath, "%s/%s", dir, cmd);
+		if (access(commandPath, X_OK) == 0)
+		{
+			execute_command(commandPath, args);
+			free(path);
+			return (1);
+		}
+		dir = strtok(NULL, ":");
+	}
+free(path);
+return (0);
 }
