5db2a1947150de5eb67f95de3c9a957ea4f5aa05
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 21:08:04 -0800

    commenting on execute file

--- execute_command.c
+++ execute_command.c
@@ -10,20 +10,23 @@ int execute_command(char *command, char **args)
 	pid_t pid;
 	int status = 0;
 
+	/* Create a child process */
 	pid = fork();
-	if (pid == 0)
+	if (pid == 0) /* Child process */
 	{
+		/* Execute the command with given arguments */
 		execve(command, args, NULL);
 		perror("execve");
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); 
 	}
-	else if (pid > 0)
+	else if (pid > 0) /* Parent process */
 	{
+		/* Wait for the child process to complete */
 		waitpid(pid, &status, 0);
 	}
-	else
+	else /* fork */
 	{
-		perror("fork");
+		perror("fork"); /* Print error if fork fails */
 	}
-return (status);
+return (status); /* Return the status of the command execution */
 }
