96fcea27f2ca084c87a9be57f5c917a0361778c2
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-06 19:47:10 -0800

    adding comments to check path

--- check_path_execute.c
+++ check_path_execute.c
@@ -11,18 +11,22 @@ int check_path_and_execute(const char *cmd, char **args)
 	char *dir;
 	char commandPath[1024];
 
+	/* Tokenize PATH to individual directories */
 	dir = strtok(path, ":");
 	while (dir != NULL)
 	{
+		/* Construct full path for the command */
 		sprintf(commandPath, "%s/%s", dir, cmd);
+
+		/* Check if the command is executable in the directory */
 		if (access(commandPath, X_OK) == 0)
 		{
 			execute_command(commandPath, args);
 			free(path);
 			return (1);
 		}
-		dir = strtok(NULL, ":");
+		dir = strtok(NULL, ":"); /* Move to the next directory in PATH */
 	}
 free(path);
-return (0);
+return (0); /* Command not found */
 }
